deb462413eea899a64036fb2ae4d32833924d9fd
pawelekbyra <pawelek.byra@gmail.com>
2026-01-26 00:36:55 +0100

    Update ArticleVideoPlayer.tsx

--- components/ArticleVideoPlayer.tsx
+++ components/ArticleVideoPlayer.tsx
@@ -24,23 +24,18 @@ const ArticleVideoPlayer: React.FC<ArticleVideoPlayerProps> = ({ src, poster }) 
   }, [src]);
 
   return (
-    <div className="my-12 w-full">
-      {/* POPRAWKA: Zastosowano 'padding-bottom hack'.
-         1. relative + paddingBottom: '56.25%' tworzy sztywną ramkę o proporcjach 16:9.
-         2. Wideo jest pozycjonowane absolutnie (absolute inset-0), co zmusza je do 
-            wypełnienia ramki co do piksela, eliminując paski i ucinanie kontrolek.
+    <div className="my-12 w-full bg-black rounded-sm shadow-lg overflow-hidden">
+      {/* POPRAWKA: Usunięto wszelkie wymuszanie proporcji (padding/aspect-ratio).
+          Użyto 'w-full h-auto', co sprawia, że wysokość odtwarzacza 
+          dostosuje się automatycznie do rzeczywistych wymiarów wideo. 
+          Klasa 'block' usuwa ewentualne dolne marginesy liniowe.
       */}
-      <div 
-        className="relative w-full bg-black rounded-sm shadow-lg overflow-hidden group"
-        style={{ paddingBottom: '56.25%' }} // 9 / 16 = 0.5625 czyli 56.25%
-      >
-        <video
-          ref={videoRef}
-          controls
-          poster={poster}
-          className="absolute top-0 left-0 w-full h-full opacity-90 group-hover:opacity-100 transition-opacity"
-        />
-      </div>
+      <video
+        ref={videoRef}
+        controls
+        poster={poster}
+        className="w-full h-auto block"
+      />
     </div>
   );
 };
