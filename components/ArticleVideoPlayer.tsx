2ddaa68d954d51d6b58253d93b9a9e959a2a6e40
pawelekbyra <pawelek.byra@gmail.com>
2026-01-26 00:33:58 +0100

    Update ArticleVideoPlayer.tsx

--- components/ArticleVideoPlayer.tsx
+++ components/ArticleVideoPlayer.tsx
@@ -24,20 +24,21 @@ const ArticleVideoPlayer: React.FC<ArticleVideoPlayerProps> = ({ src, poster }) 
   }, [src]);
 
   return (
-    <div className="my-12">
-      {/* POPRAWKA:
-          1. Zmieniono aspectRatio na '16 / 9', aby pasowało do standardu wideo i mieściło kontrolki.
-          2. Zmieniono object-cover na object-contain, aby wideo zawsze mieściło się w ramce bez ucinania krawędzi.
+    <div className="my-12 w-full">
+      {/* POPRAWKA: Zastosowano 'padding-bottom hack'.
+         1. relative + paddingBottom: '56.25%' tworzy sztywną ramkę o proporcjach 16:9.
+         2. Wideo jest pozycjonowane absolutnie (absolute inset-0), co zmusza je do 
+            wypełnienia ramki co do piksela, eliminując paski i ucinanie kontrolek.
       */}
       <div 
-        className="w-full bg-black rounded-sm shadow-lg overflow-hidden relative group"
-        style={{ aspectRatio: '16 / 9' }}
+        className="relative w-full bg-black rounded-sm shadow-lg overflow-hidden group"
+        style={{ paddingBottom: '56.25%' }} // 9 / 16 = 0.5625 czyli 56.25%
       >
         <video
           ref={videoRef}
           controls
           poster={poster}
-          className="w-full h-full object-contain opacity-90 group-hover:opacity-100 transition-opacity"
+          className="absolute top-0 left-0 w-full h-full opacity-90 group-hover:opacity-100 transition-opacity"
         />
       </div>
     </div>
