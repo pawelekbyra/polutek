0cd98f84d396de264ffd7d142823c2fc36b78aa4
pawelekbyra <pawelek.byra@gmail.com>
2026-01-30 00:43:01 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -587,7 +587,7 @@ export default function Home() {
             />
 
             <p>
-              Oznacza to, że nie jeden lecz obaj legendarni założyciele <strong>CD Projekt</strong>, na czeskim pograniczu posiadali nieruchomosci, w których odpłatnie oferowano substancje zakazane. 
+              Oznacza to, że nie jeden lecz obaj legendarni założyciele <strong>CD Projekt</strong>, na czeskim pograniczu posiadali nieruchomosci, w których odpłatnie oferowano te same substancje zakazane. 
             </p>
 
             <p>
