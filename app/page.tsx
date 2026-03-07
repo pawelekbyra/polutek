2f2428daaaa67b90ff40c7b1cd9d37666ffcf97e
pawelekbyra <pawelek.byra@gmail.com>
2026-03-07 22:58:59 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -632,7 +632,7 @@ export default function Page() {
               <h2 className="not-prose section-heading text-4xl font-black tracking-tighter text-black uppercase border-b-4 border-black mb-6 mt-16 font-display">Wiktor B.</h2>
 
               <p className="mt-4">
-                9. marca 2026 Onet ujawnia opinii publicznej wstrząsające kulisy działalności ośrodka w czeskim Janowie. 🌐 Dziennikarskie śledztwo koncentruje się na śmierci 54-letniej uczestniczki, Ilony L.-H.
+                9.03.2026 Onet ujawnia opinii publicznej wstrząsające kulisy działalności ośrodka w czeskim Janowie. 🌐 Dziennikarskie śledztwo koncentruje się na śmierci 54-letniej uczestniczki, Ilony L.-H.
               </p>
 
               <p className="mt-4">
